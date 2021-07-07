9cef500c6a596963df1167f143986bdbca8d7ed0
Tanvi Nanda <tanvi789@gmail.com>
2021-07-07 02:51:50 +0100

    fixed the css template

--- functions/server.js
+++ functions/server.js
@@ -11,55 +11,55 @@ const app = express();
 const router = express.Router();
 
 
-nunjucks.configure('views', {
-    autoescape: true,
-    express   : app
-});
-
-app.use(session({
-    secret: crypto.randomBytes(20).toString("hex"),
-    resave: true,
-    saveUninitialized: true
-}));
-app.use(bodyParser.urlencoded({extended : true}));
-app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname,'../','static')));
-
-
-// router.get('/', (req, res) => {
-//     res.writeHead(200, { 'Content-Type': 'text/html' });
-//     res.write('<h1>Hello from Express.js!</h1>');
-//     res.end();
+// nunjucks.configure('views', {
+//     autoescape: true,
+//     express   : app
 // });
+//
+// app.use(session({
+//     secret: crypto.randomBytes(20).toString("hex"),
+//     resave: true,
+//     saveUninitialized: true
+// }));
+// app.use(bodyParser.urlencoded({extended : true}));
+// app.use(bodyParser.json());
+// app.use(express.static(path.join(__dirname,'../','static')));
 
-app.get('/', function(request, response) {
-    //path.join(__dirname,'../','views');
-    response.render('index.html');
-});
 
-app.get('/register', function(request, response) {
-    path.join(__dirname,'../','views');
-    response.render('register.html');
-});
-
-app.get('/home', function(request, response) {
-    path.join(__dirname,'../','views');
-    // Check if user is logged in
-    if (request.session.loggedin) {
-        // Render home page
-        response.render('home.html', { username: request.session.username });
-    } else {
-        // Redirect to login page
-        response.redirect('/');
-    }
-});
-
-app.get('/logout', function(request, response) {
-    // Destroy session data
-    request.session.destroy();
-    // Redirect to login page
-    response.redirect('/');
-});
+// // router.get('/', (req, res) => {
+// //     res.writeHead(200, { 'Content-Type': 'text/html' });
+// //     res.write('<h1>Hello from Express.js!</h1>');
+// //     res.end();
+// // });
+//
+// app.get('/', function(request, response) {
+//     //path.join(__dirname,'../','views');
+//     response.render('index.html');
+// });
+//
+// app.get('/register', function(request, response) {
+//     path.join(__dirname,'../','views');
+//     response.render('register.html');
+// });
+//
+// app.get('/home', function(request, response) {
+//     path.join(__dirname,'../','views');
+//     // Check if user is logged in
+//     if (request.session.loggedin) {
+//         // Render home page
+//         response.render('home.html', { username: request.session.username });
+//     } else {
+//         // Redirect to login page
+//         response.redirect('/');
+//     }
+// });
+//
+// app.get('/logout', function(request, response) {
+//     // Destroy session data
+//     request.session.destroy();
+//     // Redirect to login page
+//     response.redirect('/');
+// });
 
 // router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 // router.post('/', (req, res) => res.json({ postBody: req.body }));
